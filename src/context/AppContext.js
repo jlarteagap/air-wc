c8a3e49e16bb8250c3e809c1b6fe8a969a5aee1a
Jorge <jorgelarteagap@gmail.com>
2022-04-13 22:30:05 -0400

    [FEAT] Configure context and define api

---
+++ src/context/AppContext.js
@@ -0,0 +1,26 @@
+import React, { createContext, useEffect, useState } from 'react'
+
+export const AppContext = createContext({
+  empresa: undefined
+})
+
+const { API_HOST, A, E, UB } = window.CONFIG
+
+export function AppProvider({ children }) {
+  const [empresa, setEmpresa] = useState([])
+
+  useEffect(() => {
+    const url = `${API_HOST}/empresa/?a=${A}&e=${E}&ub=${UB}`
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        setEmpresa(data.empresa)
+      })
+  }, [])
+
+  const value = {
+    empresa
+  }
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>
+}
