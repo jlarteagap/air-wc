9a4af92c9b0755092750ae239b8b71ce3cca602b
Jorge <jorgelarteagap@gmail.com>
2022-04-19 11:44:17 -0400

    [FIX] Menu top

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -50,28 +50,18 @@ function Header() {
 
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
-            <a className="navbar-item">Home</a>
+            <a className="header__navbar-item navbar-item">Home</a>
 
-            <a className="navbar-item">Documentation</a>
-
-            <div className="navbar-item has-dropdown is-hoverable">
-              <a className="navbar-link">More</a>
-
-              <div className="navbar-dropdown">
-                <a className="navbar-item">About</a>
-                <a className="navbar-item">Jobs</a>
-                <a className="navbar-item">Contact</a>
-                <hr className="navbar-divider" />
-                <a className="navbar-item">Report an issue</a>
-              </div>
-            </div>
+            <a className="header__navbar-item navbar-item">Documentation</a>
           </div>
 
           <div className="navbar-end is-align-items-center">
             <div className="menu__hidden mr-5">
               <HiOutlineMenuAlt3
                 size={24}
-                className="menu__hidden-icon"
+                className={`menu__hidden-icon ${
+                  bgMenu && 'menu__hidden-icon-color'
+                }`}
                 onClick={() => {
                   setMenu(!menu)
                 }}
